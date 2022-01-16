357e5b12b7dc1b68887edbefd063ed9ddb063b92
minhchi2494 <minhchi2494@gmail.com>
2022-01-16 19:18:38 +0700

    Add function details

---
+++ WebAPI/Controllers/CustomerAttributeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Model;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerAttributeController : ControllerBase
+    {
+        private readonly ICustomerAttributeService _service;
+
+        public CustomerAttributeController(ICustomerAttributeService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public Task<List<CustomerAttributeModel>> getAll([FromQuery] CustomerSearch customerSearch)
+        {
+            return _service.getAll(customerSearch);
+        }
+
+        [HttpGet("{id}")]
+        public Task<CustomerAttributeModel> getOne(int id)
+        {
+            return _service.getOne(id);
+        }
+
+        [HttpPost]
+        public Task<bool> create(CustomerAttributeModel newCust)
+        {
+            return _service.Create(newCust);
+        }
+
+        [HttpPut]
+        public Task<bool> edit(CustomerAttributeModel editCust)
+        {
+            return _service.Update(editCust);
+        }
+
+        [HttpDelete("{id}")]
+        public Task<bool> delete(int id)
+        {
+            return _service.Delete(id);
+        }
+    }
+}
---
+++ WebAPI/Services/ICustomerAttributeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Repository;
+using WebAPI.Model;
+
+namespace WebAPI.Services
+{
+    public interface ICustomerAttributeService 
+    {
+        Task<List<CustomerAttributeModel>> getAll(CustomerSearch customerSearch);
+        Task<CustomerAttributeModel> getOne(int id);
+        Task<bool> Create(CustomerAttributeModel newCust);
+        Task<bool> Update(CustomerAttributeModel editCust);
+        Task<bool > Delete(int id);
+    }
+}
